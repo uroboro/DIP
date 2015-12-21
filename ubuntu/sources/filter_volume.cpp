a5e754fae2f8d374d427f5a77527669928e57d69
uroboro <uroboro845@gmail.com>
2015-12-21 02:41:00 -0300

    [ubuntu] Added width and height to CVSHOW macro

--- ubuntu/sources/filter_volume.cpp
+++ ubuntu/sources/filter_volume.cpp
@@ -3,12 +3,15 @@
 #include <opencv2/imgproc/imgproc_c.h>
 #include <opencv2/highgui/highgui_c.h>
 
-#define CVSHOW(name, x, y, image) { cvNamedWindow(name, 0); cvMoveWindow(name, x, y); cvShowImage(name, image); }
+#include "fixes.h"
+
+#define CVSHOW(name, x, y, w, h, image) { cvNamedWindow(name, 0); cvMoveWindow(name, x, y); cvResizeWindow(name, w, h); cvShowImage(name, image); }
 
 int recursiveContoursDescription(CvSeq *contours, int full_area, int indent) {
 	int area_total = 0;
 	int total = 0; for (CvSeq *tmp = contours; tmp != NULL; tmp = tmp->h_next, total++);
 	CvSeq *tmp = contours;
+
 	for (int i = 0; tmp != NULL; tmp = tmp->h_next, i++) {
 		for (int t = 0; t < indent; t++) printf(" ");
 		int area = (int)cvContourArea(tmp, CV_WHOLE_SEQ, 0);
@@ -16,6 +19,7 @@ int recursiveContoursDescription(CvSeq *contours, int full_area, int indent) {
 		area_total += area;
 		area_total += recursiveContoursDescription(tmp->v_next, full_area, indent + 1);
 	}
+
 	return area_total;
 }
 int recursiveContoursDraw(IplImage *dst, CvSeq *contours, int indent) {
@@ -35,7 +39,7 @@ int filterByVolume(IplImage *src, IplImage *dst, long minVolume) {
 {
 	IplImage *tmp3d = cvCreateImage(cvGetSize(src), IPL_DEPTH_8U, 3);
 	cvMerge(src, src, src, NULL, tmp3d);
-	CVSHOW("orig0", 0, 0, tmp3d);
+	CVSHOW("orig0", 0, 0, 200, 200, tmp3d);
 
 	IplImage *tmp1d = cvCloneImage(src);
 	CvMemStorage *storage = cvCreateMemStorage(0);
@@ -49,16 +53,16 @@ int filterByVolume(IplImage *src, IplImage *dst, long minVolume) {
 	recursiveContoursDraw(tmp3d, contours, 1);
 
 	cvReleaseMemStorage(&storage);
-	CVSHOW("orig1", 0, 300, tmp3d);
+	CVSHOW("orig1", 0, 250, 200, 200, tmp3d);
 	cvReleaseImage(&tmp3d);
 	printf("\n");
 }
-if (01)
+if (0)
 {
 	IplImage *tmp3d = cvCreateImage(cvGetSize(src), IPL_DEPTH_8U, 3);
 	cvMerge(src, src, src, NULL, tmp3d);
 	cvThreshold(tmp3d, tmp3d, 127, 255, CV_THRESH_BINARY_INV);
-	CVSHOW("flip0", 600, 0, tmp3d);
+	CVSHOW("flip0", 500, 0, 200, 200, tmp3d);
 
 	IplImage *tmp1d = cvCloneImage(src);
 	cvThreshold(tmp1d, tmp1d, 127, 255, CV_THRESH_BINARY_INV);
@@ -73,7 +77,7 @@ if (01)
 	recursiveContoursDraw(tmp3d, contours, 1);
 
 	cvReleaseMemStorage(&storage);
-	CVSHOW("flip1", 600, 300, tmp3d);
+	CVSHOW("flip1", 500, 250, 200, 200, tmp3d);
 	cvReleaseImage(&tmp3d);
 	printf("\n");
 }
