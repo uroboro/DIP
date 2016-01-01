2c23abe8c4174b8a2e3cb840333a8587d7de3469
uroboro <uroboro845@gmail.com>
2016-01-01 20:25:30 -0300

    [ubuntu] Added definitions for different platforms

--- ubuntu/sources/common.h
+++ ubuntu/sources/common.h
@@ -11,11 +11,26 @@
 #define DIP_EXTERN_END
 #endif
 
-#ifdef __OBJC__
+#if __OBJC__
 #include <objc/runtime.h>
 #import <UIKit/UIAlertView.h>
 
 #define UIAlert(t, m) dispatch_async(dispatch_get_main_queue(), ^{ [[[[objc_getClass("UIAlertView") alloc] initWithTitle:(t) message:(m) delegate:0 cancelButtonTitle:@"OK" otherButtonTitles:0] autorelease] show]; })
+#endif
+
+#if defined(_WIN16) || defined(_WIN32) || defined(_WIN64) || defined(__WIN32__) || defined(__TOS_WIN__) || defined(__WINDOWS__)
+#define __windows__ 1
+#endif
+
+
+#if __linux__ || __windows__
+#define CVSHOW(name, x, y, w, h, image) { cvNamedWindow(name, 0); if (x > 0 && y > 0) cvMoveWindow(name, x, y); cvResizeWindow(name, w, h); cvShowImage(name, image); }
+#endif
+
+#if __windows__
+#define RESOURCES "C:\\Users\\uroboro\\Documents\\GitHub\\DIP\\DIP\\resources\\"
+#else
+#define RESOURCES "resources/"
 #endif
 
 #endif /* COMMON_H */
