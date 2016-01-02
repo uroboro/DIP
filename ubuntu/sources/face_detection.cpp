26f5f99213e4d85b33489d25debdf77a8e19b7ad
uroboro <uroboro845@gmail.com>
2016-01-02 00:57:08 -0300

    [ubuntu] Removed unnecessary code, formatting and more verbosity
    * Added grayscale guard to work with bitmaps only.
    * `Fixed` cascade crash on dealloc when returning from the function by adding the static modifier. Made arguments into variables. Can't use C since the classifier XML is built for the C++ implementation.
    * Simplified drawing code by using an alternative cvRectangle.

--- ubuntu/sources/face_detection.cpp
+++ ubuntu/sources/face_detection.cpp
@@ -4,78 +4,73 @@
 #include <opencv2/imgproc/imgproc_c.h>
 #include <opencv2/objdetect/objdetect.hpp>
 
-#ifdef __WIN__
-#define RESOURCES "C:\\Users\\uroboro\\Documents\\GitHub\\DIP\\DIP\\resources\\"
-#else
-#define RESOURCES "/home/uroboro/Documents/GitHub/DIP/DIP/resources/"
-#endif
-#define LS printf("%s::%d\n", __PRETTY_FUNCTION__, __LINE__);
+#include "fixes.h"
 
 int findFaces(CvArr* src, CvRect **rects, double scale, double *t) {
-	CvSize srcSize = cvGetSize(src);
-	IplImage *gray = cvCreateImage(srcSize, IPL_DEPTH_8U, 1);
-	cvCvtColor(src, gray, CV_BGR2GRAY);
+	// Make image grayscale
+	IplImage *tmp1d = NULL;
+	if (((IplImage *)src)->nChannels == 1) {
+		tmp1d = cvCloneImage((IplImage *)src);
+	} else {
+		tmp1d = cvCreateImage(cvGetSize(src), ((IplImage *)src)->depth, 1);
+		cvCvtColor(src, tmp1d, CV_BGR2GRAY);
+	}
 
+	// Scale down image
+	CvSize srcSize = cvGetSize(src);
 	double fx = 1 / scale;
 	srcSize.width = (int)cvRound(fx * srcSize.width);
 	srcSize.height = (int)cvRound(fx * srcSize.height);
 
 	IplImage *smallImg = cvCreateImage(srcSize, IPL_DEPTH_8U, 1);
-	cvResize(gray, smallImg, CV_INTER_LINEAR);
-	cvReleaseImage(&gray);
+	cvResize(tmp1d, smallImg, CV_INTER_LINEAR);
+	cvReleaseImage(&tmp1d);
 
 	cvEqualizeHist(smallImg, smallImg);
 
-	char frontalFaceFile[] = RESOURCES "haarcascade_frontalface_alt.xml";
-
-LS;	cv::CascadeClassifier cascade;
-	if (!cascade.load(frontalFaceFile)) {
-		printf("ERROR: Could not load classifier cascade\r");
-		return -1;
-	}
-
-	std::vector<cv::Rect> faces;
-	*t = (double)cvGetTickCount();
-LS;	cascade.detectMultiScale(smallImg, faces,
-		1.1, 2, 0
+	double scale_factor = 1.1;
+	int min_neighbors = 3;
+	int flags = 0
 		//|CASCADE_FIND_BIGGEST_OBJECT
 		//|CASCADE_DO_ROUGH_SEARCH
-		| cv::CASCADE_SCALE_IMAGE,
-		cv::Size(30, 30));
+		| cv::CASCADE_SCALE_IMAGE;
+	CvSize min_size = cvSize(0,0);
+	CvSize max_size = cvSize(100,100);
+	std::vector<cv::Rect> objects;
+	static cv::CascadeClassifier cascade;
+	if (cascade.empty() && !cascade.load("resources/haarcascade_frontalface_alt.xml")) {
+		printf("ERROR: Could not load classifier cascade\n");
+		return -1;
+	}
+	double t_diff = (double)cvGetTickCount();
+	cascade.detectMultiScale(smallImg, objects, scale_factor, min_neighbors, flags, min_size, max_size);
+	*t = ((double)cvGetTickCount() - t_diff) / (cvGetTickFrequency() * 1000);
+	cvReleaseImage(&smallImg);
 
-	*t = (double)cvGetTickCount() - *t;
-	*t /= ((double)cvGetTickFrequency() * 1000);
+	int size = objects.size();
+	*rects = (CvRect *)calloc(size + 1, sizeof(CvRect));
+	for (int i = 0; i < size; i++) {
+		CvRect face = objects[i];
+		(*rects)[i] = cvRect(cvRound(face.x * scale), cvRound(face.y * scale), cvRound(face.width * scale), cvRound(face.height * scale));
+	}
 
-	*rects = (CvRect *)calloc(faces.size() + 1, sizeof(CvRect));
-LS;	for (size_t i = 0; i < faces.size(); i++) {
-		CvRect face = faces[i];
-		face = cvRect(cvRound(face.x * scale), cvRound(face.y * scale), cvRound(face.width * scale), cvRound(face.height * scale));
-		(*rects)[i] = face;
-LS;	}
-
-LS;	int size = faces.size();
-LS;	return size;
+	return size;
 }
 
 void drawFaces(CvArr *dst, size_t facesCount, CvRect *faces) {
 	const static CvScalar colors[] =
 	{
-		cvScalar(255, 0, 0),
-		cvScalar(255, 128, 0),
-		cvScalar(255, 255, 0),
-		cvScalar(0, 255, 0),
-		cvScalar(0, 128, 255),
-		cvScalar(0, 255, 255),
-		cvScalar(0, 0, 255),
-		cvScalar(255, 0, 255)
+		cvScalar(255, 000, 000),
+		cvScalar(255, 128, 000),
+		cvScalar(255, 255, 000),
+		cvScalar(000, 255, 000),
+		cvScalar(000, 128, 255),
+		cvScalar(000, 255, 255),
+		cvScalar(000, 000, 255),
+		cvScalar(255, 000, 255)
 	};
 
 	for (size_t i = 0; i < facesCount; i++) {
-		CvRect r = faces[i];
-		CvScalar color = colors[i % 8];
-
-		cvRectangle(dst, cvPoint(r.x, r.y),
-			cvPoint(r.x + r.width - 1, r.y + r.height - 1),
-			color, 3, 8, 0);
+		cvRectangle2(dst, faces[i], colors[i % 8], 3, 8, 0);
 	}
 }
