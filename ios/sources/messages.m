57c15174aa9070da376b5c825d37a68e0fb16579
uroboro <uroboro845@gmail.com>
2016-01-09 21:46:11 -0300

    [ios] Started work on making messages.h crossplatform

--- ios/sources/messages.m
+++ ios/sources/messages.m
@@ -3,15 +3,20 @@
 #include <unistd.h>
 #include <stdarg.h>
 #include <syslog.h>
+
+#if __OBJC__
 #include <objc/message.h>
 #include <CoreFoundation/CFNotificationCenter.h>
+#endif
 
-#define UIAlert(t, m) dispatch_async(dispatch_get_main_queue(), ^{ [[[[objc_getClass("UIAlertView") alloc] initWithTitle:(t) message:(m) delegate:0 cancelButtonTitle:(id)CFSTR("OK") otherButtonTitles:0] autorelease] show]; })
+#include "messages.h"
 
 typedef enum DBGOutputMode {
-	DBGOutputModeGUI,
-	DBGOutputModeTTY,
-	DBGOutputModeSyslog
+	DBGOutputModeTTY    = 1,
+	DBGOutputModeSyslog = 2,
+#if __OBJC__
+	DBGOutputModeGUI    = 3
+#endif
 } DBGOutputMode;
 
 static DBGOutputMode outputMode = DBGOutputModeTTY;
@@ -20,40 +25,48 @@ int present(char mode, const char format[], ...) {
 	int r = 0;
 	if (!format) { present(1, "!format"); return r; }
 
+	char message[1024] = { 0 };
+
 	va_list args;
 	va_start(args, format);
-//	const char *syslogPrefix = "\x1b[1;34m[IRCyslog]\x1b[0m";
-	NSString *message = [[NSString alloc] initWithFormat:@(format) arguments:args];
+	vsprintf(message, format, args);
 	va_end(args);
 
 	switch (outputMode) {
-	case DBGOutputModeGUI:
-		UIAlert((mode ? @"ERROR" : @"WARNING"), message);
-		break;
 	case DBGOutputModeTTY:
-		r = fprintf(mode ? stderr : stdout, "%s\n", message.UTF8String);
+		r = fprintf(mode ? stderr : stdout, "%s\n", message);
 		break;
 	case DBGOutputModeSyslog:
-		syslog(mode ? LOG_ERR : LOG_WARNING, "%s", message.UTF8String);
+		syslog(mode ? LOG_ERR : LOG_WARNING, "%s", message);
 		break;
+#if __OBJC__
+	case DBGOutputModeGUI: {
+			NSString *m = [NSString stringWithUTF8String:message];
+			UIAlert((mode ? @"ERROR" : @"WARNING"), m);
+		}
+		break;
+#endif
 	}
-	[message release];
 
 	return r;
 }
 
+#if __OBJC__
 static void callback(CFNotificationCenterRef center, void *observer, CFStringRef name, const void *object, CFDictionaryRef userInfo) {
 	outputMode = DBGOutputModeGUI; 
 }
+#endif
 
 __attribute__((constructor)) static void ctor(int argc, char **argv, char **envp) {
-	outputMode = ([UIApplication sharedApplication]) ? DBGOutputModeGUI : (isatty(STDIN_FILENO)) ? DBGOutputModeTTY : DBGOutputModeSyslog;
+	outputMode = (isatty(STDIN_FILENO)) ? DBGOutputModeTTY : DBGOutputModeSyslog;
+
+#if __OBJC__
+	outputMode = ([UIApplication sharedApplication]) ? DBGOutputModeGUI : outputMode;
 
 	CFNotificationCenterAddObserver(
 		CFNotificationCenterGetLocalCenter(),
 		NULL, callback,
 		(CFStringRef)UIApplicationDidFinishLaunchingNotification,
 		NULL, CFNotificationSuspensionBehaviorCoalesce);
-
-	return;
+#endif
 }
