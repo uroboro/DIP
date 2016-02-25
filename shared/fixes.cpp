ad77f8724fde6269703d07ee6cb750323b3d1a36
uroboro <uroboro845@gmail.com>
2016-02-25 19:43:01 -0300

    [shared] Added C bindings for C++ only functions

---
+++ shared/fixes.cpp
@@ -0,0 +1,9 @@
+#include "fixes.hpp"
+
+double getTickCount() {
+	return cv::getTickCount();
+}
+
+double getTickFrequency() {
+    return cv::getTickFrequency();
+}
---
+++ shared/fixes.hpp
@@ -0,0 +1,12 @@
+#ifndef FIXES_HPP
+#define FIXES_HPP
+
+#include <opencv2/core/core_c.h>
+
+#include "common.h"
+
+DIP_EXTERN double getTickCount();
+
+DIP_EXTERN double getTickFrequency();
+
+#endif /* FIXES_HPP */
