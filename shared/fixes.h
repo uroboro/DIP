07261858141040ed3f3b5ce22757197342f7accd
uroboro <uroboro845@gmail.com>
2016-03-07 12:19:16 -0300

    [shared] Changed to block extern

--- shared/fixes.h
+++ shared/fixes.h
@@ -5,28 +5,32 @@
 
 #define cvReleaseMemStorage2(sequence) if (sequence && sequence->storage) { cvReleaseMemStorage(&sequence->storage); }
 
-DIP_EXTERN void cvCopy2(CvArr *src, CvArr *dst, CvArr *mask);
+DIP_EXTERN_BEGIN
 
-DIP_EXTERN void cvCopyNonZero(CvArr *src, CvArr *dst, CvArr *mask);
+void cvCopy2(CvArr *src, CvArr *dst, CvArr *mask);
 
-DIP_EXTERN int cvFindContours2(IplImage* image, CvMemStorage* storage, CvSeq** first_contour, int header_size, int mode, int method, CvPoint offset);
+void cvCopyNonZero(CvArr *src, CvArr *dst, CvArr *mask);
 
-DIP_EXTERN void cvTranslateImage2(IplImage *src, IplImage *dst, int offsetx, int offsety);
+int cvFindContours2(IplImage* image, CvMemStorage* storage, CvSeq** first_contour, int header_size, int mode, int method, CvPoint offset);
 
-DIP_EXTERN int cvContourArea2(CvSeq *contour);
+void cvTranslateImage2(IplImage *src, IplImage *dst, int offsetx, int offsety);
 
-DIP_EXTERN IplImage* createSubArray(IplImage *src, CvRect rect);
+int cvContourArea2(CvSeq *contour);
 
-DIP_EXTERN void cvRectangle2(CvArr* img, CvRect rect, CvScalar color, int thickness, int line_type, int shift);
+IplImage* createSubArray(IplImage *src, CvRect rect);
 
-DIP_EXTERN void cvFillConvexPoly2(CvArr* img, CvSeq *points, CvScalar color, int line_type, int shift);
+void cvRectangle2(CvArr* img, CvRect rect, CvScalar color, int thickness, int line_type, int shift);
 
-DIP_EXTERN void cvBox2(CvArr* img, CvBox2D rect, CvScalar color, int thickness, int line_type, int shift);
+void cvFillConvexPoly2(CvArr* img, CvSeq *points, CvScalar color, int line_type, int shift);
 
-DIP_EXTERN void ocvResizeFrame(IplImage *src, IplImage *dst);
+void cvBox2(CvArr* img, CvBox2D rect, CvScalar color, int thickness, int line_type, int shift);
 
-DIP_EXTERN void ocv2DAffineMatrix(CvMat* map_matrix, CvPoint2D32f c, float a);
+void ocvResizeFrame(IplImage *src, IplImage *dst);
 
-DIP_EXTERN void cvClose(CvArr *src, CvArr *dst, CvArr *mask, size_t n);
+void ocv2DAffineMatrix(CvMat* map_matrix, CvPoint2D32f c, float a);
+
+void cvClose(CvArr *src, CvArr *dst, CvArr *mask, size_t n);
+
+DIP_EXTERN_END
 
 #endif /* FIXES_H */
