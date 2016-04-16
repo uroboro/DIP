c65a5a0e500fa5fb6d32c7729550ff982bc06272
uroboro <uroboro845@gmail.com>
2016-04-16 17:55:13 -0300

    [shared] Reused function and removed unused code

--- shared++/filter_grayscale.cpp
+++ shared++/filter_grayscale.cpp
@@ -1,14 +1,5 @@
 #include "filter_grayscale.hpp"
 
-//#include "try.h"
-//#include "fixes2.hpp"
-
-#if DIP_MOBILE
-//#include <dispatch/dispatch.h>
-#else
-//#define dispatch_async(...)
-#endif
-
 static void ocvDistance2GrayscaleFunc(cv::Vec3b& p, const int *pos) {
 	p = cv::Vec3b::all(sqrt(2.0 / 3 * (
 		  p[0] * (p[0] - p[1])
@@ -18,24 +9,18 @@ static void ocvDistance2GrayscaleFunc(cv::Vec3b& p, const int *pos) {
 }
 
 void ocvDistance2GrayscaleMat(cv::Mat& src, cv::Mat& dst) {
-	#if CV_VERSION_MAJOR >= 3
-		src.copyTo(dst);
-		dst.forEach<cv::Vec3b>(&ocvDistance2GrayscaleFunc);
-	#else
-		if (src.channels() == 3) {
+	src.copyTo(dst);
+	if (src.channels() == 3) {
+		#if CV_VERSION_MAJOR >= 3
+			dst.forEach<cv::Vec3b>(&ocvDistance2GrayscaleFunc);
+		#else
 			for (unsigned int y = 0; y < src.size().height; y++) {
 				for (unsigned int x = 0; x < src.size().width; x++) {
-					cv::Vec3b p = src.at<cv::Vec3b>(cv::Point(x, y));
-					// Calculate distance to grayscale value
-					dst.at<cv::Vec3b>(cv::Point(x, y)) = cv::Vec3b::all(sqrt(2.0 / 3 * (
-						  p[0] * (p[0] - p[1])
-						+ p[1] * (p[1] - p[2])
-						+ p[2] * (p[2] - p[0])
-					)));
+					ocvDistance2GrayscaleFunc(dst.at<cv::Vec3b>(cv::Point(x, y)), NULL);
 				}
 			}
-		}
-	#endif
+		#endif
+	}
 }
 
 void maskByDistance2GrayscaleMat(cv::Mat& src, cv::Mat& dst, int minDistance) {
